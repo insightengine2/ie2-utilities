97f862966cd1bfd5f4e2cf0ad91ed61638531311
Gregory Baker <gregory.baker@duke.edu>
2024-09-10 12:54:58 -0400

    Expanded FileMetaData Types
    Motivation
    ---
    The FileMetaData defined custom types within its own declartion for Author and ResearchArea. This change moves those into their own definitions so they're explicit and reuseable.
    
    Modifications
    ---
    - Defined explicit Author and ResearchArea types

--- types/filemetadata.go
+++ types/filemetadata.go
@@ -1,16 +1,19 @@
 package ie2datatypes
 
+type Author struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+type ResearchArea struct {
+	Name string `json:"name"`
+}
+
 type FileMetaData struct {
-	OGFileName string `json:"ogfilename"`
-	Synopsis   string `json:"synopsis"`
-	Authors    []struct {
-		FirstName string `json:"firstname"`
-		LastName  string `json:"lastname"`
-	} `json:"authors"`
-	ResearchAreas []struct {
-		ID   int32  `json:"id"`
-		Name string `json:"name"`
-	} `json:"researchareas"`
-	UploadedOn string `json:"uploadedon"`
-	IngestedOn string `json:"ingestedon"`
+	OGFileName    string         `json:"ogfilename"`
+	Synopsis      string         `json:"synopsis"`
+	Authors       []Author       `json:"authors"`
+	ResearchAreas []ResearchArea `json:"researchareas"`
+	UploadedOn    string         `json:"uploadedon"`
+	IngestedOn    string         `json:"ingestedon"`
 }
