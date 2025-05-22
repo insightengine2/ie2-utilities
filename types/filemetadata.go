533d5ed072b2b51b7aff984432bc94cec43876a3
Gregory Baker <gregory.baker@duke.edu>
2025-05-22 15:04:41 -0400

    Author DB Field Maps
    Motivation
    ---
    Update the Author struct to support mapping DB fields to struct fields.
    
    Modifications
    ---
    - Updated Author struct with db tags

--- types/filemetadata.go
+++ types/filemetadata.go
@@ -7,9 +7,16 @@ type Paper struct {
 }
 
 type Author struct {
-	FirstName string  `json:"firstname"`
-	LastName  string  `json:"lastname"`
-	Papers    []Paper `json:"papers,omitempty"`
+	Id         *int    `json:"id,omitempty" db:"id"`
+	FirstName  string  `json:"firstname" db:"fname"`
+	MiddleName string  `json:"middlename,omitempty" db:"mname"`
+	LastName   string  `json:"lastname" db:"lname"`
+	Title      string  `json:"title,omitempty" db:"title"`
+	IsActive   bool    `json:"isactive,omitempty" db:"isactive"`
+	CreatedOn  string  `json:"createdon,omitempty" db:"createdOn"`
+	UpdatedOn  string  `json:"updatedon,omitempty" db:"updatedOn"`
+	DeletedOn  string  `json:"deletedon,omitempty" db:"deletedOn"`
+	Papers     []Paper `json:"papers,omitempty" db:"-"`
 }
 
 type ResearchArea struct {
