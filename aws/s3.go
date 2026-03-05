e13a9bbd4ab2ecda070bd89c6a2973acd4053737
Gregory Baker <gregory.baker@duke.edu>
2026-03-05 09:08:34 -0500

    S3 ObjectToBuff
    Motivation
    ---
    Continuing to consolidate common aws operations into a helper lib.
    
    Modifications
    ---
    - Added S3ObjectToBuff

--- aws/s3.go
+++ aws/s3.go
@@ -1,12 +1,45 @@
 package ie2aws
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
+
+func S3ObjectToBuff(obj *s3.GetObjectOutput) (*bytes.Buffer, error) {
+
+	if obj == nil {
+		return nil, errors.New("unable to read s3 file contents, obj is empty")
+	}
+
+	log.Print("Attempting to read file contents from S3...")
+
+	buffer := new(bytes.Buffer)
+
+	if *obj.ContentLength > 0 {
+
+		log.Printf("File found! Attempting to parse...")
+
+		readBytes := int64(0)
+		readBytes, err := buffer.ReadFrom(obj.Body)
+
+		if err != nil {
+			log.Printf("Error reading file!")
+			return nil, err
+		}
+
+		log.Printf("Successfully read %d bytes.", readBytes)
+
+	} else {
+		log.Printf("Unable to read file contents!")
+	}
+
+	return buffer, nil
+}
 
 func S3GetObject(conf *aws.Config, ctx *context.Context, bucket string, key string) (*s3.GetObjectOutput, error) {
 
