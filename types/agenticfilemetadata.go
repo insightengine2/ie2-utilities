b0d79e5f557b3c2838f7c5d266cc1f51570494d0
Gregory Baker <gregory.baker@duke.edu>
2026-03-23 14:16:46 -0400

    Update AgenticMetadata
    Motivation
    ---
    We want the LLM to categorize the file as well.
    
    Modifications
    ---
    - Added new field to AgenticFileMetaData

--- types/agenticfilemetadata.go
+++ types/agenticfilemetadata.go
@@ -1,13 +1,14 @@
 package ie2datatypes
 
 type AgenticFileMetaData struct {
-	Abstract    string   `json:"abstract"`
-	AIModel     string   `json:"aimodel"`
-	Authors     []string `json:"authors"`
-	CreatedOn   string   `json:"createdon"`
-	Filename    string   `json:"filename"`
-	Keywords    []string `json:"keywords"`
-	Provider    string   `json:"provider"`
-	PublishDate string   `json:"publishdate"`
-	Title       string   `json:"title"`
+	Abstract              string   `json:"abstract"`
+	AIModel               string   `json:"aimodel"`
+	Authors               []string `json:"authors"`
+	CreatedOn             string   `json:"createdon"`
+	Filename              string   `json:"filename"`
+	Keywords              []string `json:"keywords"`
+	Provider              string   `json:"provider"`
+	PublishDate           string   `json:"publishdate"`
+	ResearchSubCategories []string `json:"subcategories"`
+	Title                 string   `json:"title"`
 }
