d9e5197acc5ac4b3767c24ab319aeee3a972b7a0
Gregory Baker <gregory.baker@duke.edu>
2026-04-01 11:57:16 -0400

    Update AgenticFileMetaData
    Motivation
    ---
    The struct fieldname for researchareas does not match the filename and we want to correct that.
    
    Modifications
    ---
    - Change fieldname to researchareas

--- types/agenticfilemetadata.go
+++ types/agenticfilemetadata.go
@@ -1,14 +1,14 @@
 package ie2datatypes
 
 type AgenticFileMetaData struct {
-	Abstract              string   `json:"abstract"`
-	AIModel               string   `json:"aimodel"`
-	Authors               []string `json:"authors"`
-	CreatedOn             string   `json:"createdon"`
-	Filename              string   `json:"filename"`
-	Keywords              []string `json:"keywords"`
-	Provider              string   `json:"provider"`
-	PublishDate           string   `json:"publishdate"`
-	ResearchSubCategories []string `json:"subcategories"`
-	Title                 string   `json:"title"`
+	Abstract      string   `json:"abstract"`
+	AIModel       string   `json:"aimodel"`
+	Authors       []string `json:"authors"`
+	CreatedOn     string   `json:"createdon"`
+	Filename      string   `json:"filename"`
+	Keywords      []string `json:"keywords"`
+	Provider      string   `json:"provider"`
+	PublishDate   string   `json:"publishdate"`
+	ResearchAreas []string `json:"researchareas"`
+	Title         string   `json:"title"`
 }
